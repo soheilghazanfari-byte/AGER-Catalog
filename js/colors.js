f288f85c741f2fa1b943f1ab769a8882efa4e0cd
soheilghazanfari-byte <soheil.ghazanfari@gmail.com>
2026-07-11 13:33:29 +0330

    Update colors.js

--- js/colors.js
+++ js/colors.js
@@ -1,1 +1,74 @@
+// =====================================
+// colors.js
+// =====================================
 
+const materialColors = {
+
+    "استیل 304 دست ساز": [
+        "سیلور",
+        "طلایی PVD",
+        "مشکی مات استاتیک"
+    ],
+
+    "استیل 304 پرسی": [
+        "سیلور",
+        "طلایی PVD",
+        "مشکی مات استاتیک"
+    ],
+
+    "ABS/استیل": [
+        "سیلور",
+        "طلایی PVD",
+        "مشکی مات استاتیک"
+    ],
+
+    "برنجی": [
+        "طلایی براق",
+        "طلایی مات",
+        "رزگلد",
+        "زیتونی",
+        "سفید",
+        "مشکی"
+    ]
+
+};
+
+function loadColors() {
+
+    const material = document.getElementById("material");
+    const color = document.getElementById("color");
+    const previewMaterial = document.getElementById("previewMaterial");
+    const previewColor = document.getElementById("previewColor");
+
+    function updateColors() {
+
+        previewMaterial.textContent = material.value;
+
+        color.innerHTML = "";
+
+        materialColors[material.value].forEach(item => {
+
+            const option = document.createElement("option");
+
+            option.value = item;
+            option.textContent = item;
+
+            color.appendChild(option);
+
+        });
+
+        previewColor.textContent = color.value;
+
+    }
+
+    material.addEventListener("change", updateColors);
+
+    color.addEventListener("change", () => {
+
+        previewColor.textContent = color.value;
+
+    });
+
+    updateColors();
+
+}
