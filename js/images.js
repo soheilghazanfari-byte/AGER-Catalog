7862fb634d23a383c76fb8d41bc682f66e670c79
soheilghazanfari-byte <soheil.ghazanfari@gmail.com>
2026-07-11 13:35:20 +0330

    Update images.js

--- js/images.js
+++ js/images.js
@@ -1,1 +1,72 @@
+// =====================================
+// images.js
+// =====================================
 
+function loadImages() {
+
+    const heroInput = document.getElementById("heroInput");
+    const productInput = document.getElementById("productInput");
+
+    const hero = document.getElementById("hero");
+    const thumb = document.getElementById("thumb");
+
+    // عکس محیط
+    heroInput.addEventListener("change", function () {
+
+        const file = this.files[0];
+
+        if (!file) return;
+
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+
+            hero.innerHTML = "";
+
+            const img = document.createElement("img");
+
+            img.src = e.target.result;
+
+            img.style.width = "100%";
+            img.style.height = "100%";
+            img.style.objectFit = "cover";
+            img.style.borderRadius = "0";
+
+            hero.appendChild(img);
+
+        };
+
+        reader.readAsDataURL(file);
+
+    });
+
+    // عکس محصول
+    productInput.addEventListener("change", function () {
+
+        const file = this.files[0];
+
+        if (!file) return;
+
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+
+            thumb.innerHTML = "";
+
+            const img = document.createElement("img");
+
+            img.src = e.target.result;
+
+            img.style.width = "100%";
+            img.style.height = "100%";
+            img.style.objectFit = "contain";
+
+            thumb.appendChild(img);
+
+        };
+
+        reader.readAsDataURL(file);
+
+    });
+
+}
